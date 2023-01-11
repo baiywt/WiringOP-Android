2f67d99e3a60d7675b712179cf3405c219ce5cad
leeboby <leeboby@aliyun.com>
2023-01-11 19:21:00 +0800

    Update

--- app/src/main/java/com/example/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.wiringop;
+package com.example.demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.demo.R;
 
 public class MainActivity extends AppCompatActivity {
 
