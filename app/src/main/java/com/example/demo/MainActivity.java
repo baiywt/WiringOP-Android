80629eeaecb9a5e6ffa08afe7a8cbd5cfabc9b10
leeboby <leeboby@aliyun.com>
2023-01-12 11:58:59 +0800

    ok

--- app/src/main/java/com/example/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.demo;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.wiringop.wpiControl;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        wpiControl.wiringPiSetup();
+	wpiControl.pinMode(28, 1);
     }
 }
